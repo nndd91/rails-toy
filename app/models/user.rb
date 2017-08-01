3da78d2167f25eca10f7b91bbaef737080524e26
nndd91 <nndd91@hotmail.com>
2017-08-01 22:41:01 +0800

    Add confirmationcontroller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   def password_required?
     super if confirmed?
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'static/index'
 
   devise_scope :user do
-    put "/confirm" => "confirmations#confirm"
+    patch "/confirm" => "confirmations#confirm"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
